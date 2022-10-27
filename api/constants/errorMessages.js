cf67bbeda08c8f49579a95e935ae9ee857ce7ecf
roqeebYusuff <roqeebyusuff001@gmail.com>
2022-10-28 05:08:13 +0800

    Added Constants

---
+++ api/constants/errorMessages.js
@@ -0,0 +1,7 @@
+module.exports.errorMessages = {
+  BAD_REQUEST: "Provide all required parameters",
+  INVALID_CREDENTIALS: "Invalid credentials",
+  NOT_FOUND: "Resource not found",
+  UNAUTHORIZED: "Unauthorized",
+  UNPROCESSABLE_ENTITY: "Unprocessable entity",
+};
