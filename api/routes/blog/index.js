9e523704a95bfc00f69a874776c32eb69c98eafd
roqeebYusuff <roqeebyusuff001@gmail.com>
2022-10-29 21:10:31 +0100

    :memo: Added other blog routes

--- api/routes/blog/index.js
+++ api/routes/blog/index.js
@@ -1,6 +1,20 @@
 const { Router } = require("express");
+const {
+  createBlog,
+  allBlogs,
+  deleteBlog,
+  updateBlog,
+  getOneBlog,
+} = require("../../controllers/blog");
+const { authorize } = require("../../middleware/auth");
 
 let router = Router();
+
+router.get("/listOne/:id", getOneBlog); //Get one blog
+router.get("/list", allBlogs); //Get all blog
+router.post("/create", authorize, createBlog); //Create blog
+router.patch("/update/:id", authorize, updateBlog); //Update blog
+router.delete("/delete/:id", authorize, deleteBlog); //Delete blog
 
 router.get("/", function (req, res) {
   res.json({
