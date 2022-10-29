f6f79682911c14db82baecc7ff2a2f20c8eb52b9
roqeebYusuff <roqeebyusuff001@gmail.com>
2022-10-29 21:07:13 +0100

    :sparkles: Add signup method controller

--- api/controllers/User/index.js
+++ api/controllers/User/index.js
@@ -1,41 +1,80 @@
-const { errorCodes } = require("../../constants/errorCodes");
-const { errorMessages } = require("../../constants/errorMessages");
+const { statusCodes } = require("../../constants/statusCodes");
+const { statusMessages } = require("../../constants/statusMessages");
 const userModel = require("../../models/user");
+const { checkPassword } = require("../../utils/passwordUtils");
 
 module.exports.signin = (req, res) => {
   let { email, password } = req.body;
 
   /* Check if required fields are provided */
   if (!email || !password) {
-    return res.json({
+    return res.status(statusCodes.BAD_REQUEST).json({
       success: false,
-      errorCode: errorCodes.BAD_REQUEST,
-      message: errorMessages.INVALID_CREDENTIALS,
+      errorCode: statusCodes.BAD_REQUEST,
+      message: statusMessages.PROVIDE_REQUIRED_FIELDS,
     });
   }
 
   userModel
     .findOne({ email })
-    .select("+password")
+    .select("+password") //select password to compare
     .then((user) => {
-      res.json({
+      if (!user || !checkPassword(user["password"], password)){   
+        return res.status(statusCodes.BAD_REQUEST).json({
+          succes: false,
+          errorCode: statusCodes.BAD_REQUEST,
+          message: statusMessages.INVALID_CREDENTIALS,
+        })
+      }
+      // unselect password
+      delete user.password;
+      return res.status(statusCodes.SUCCESS).json({
+        success: true,
         user,
+        token: userModel.createSessionToken(user._id),
       });
-      //   if (!user || !passwordUtils.checkPassword(user["password"], password))
-      //     throw ErrorMessages.INVALID_CREDENTAILS;
-
-      //   delete user.password;
-      //   res.json({
-      //     success: true,
-      //     user,
-      //     token: AdminUser.createSessionToken(user._id),
-      //   });
     })
     .catch((error) => {
-      res.json({
+      return res.json({
         success: false,
-        errorCode: errorCodes.SERVER_ERROR,
+        errorCode: statusCodes.SERVER_ERROR,
         message: error,
       });
     });
+};
+
+/* User signup */
+module.exports.signup = (req, res) => {
+  let { first_name, last_name, email, username, password } = req.body;
+
+  if (!first_name || !last_name || !email || !username || !password) {
+    return res.status(statusCodes.BAD_REQUEST).json({
+      success: false,
+      errorCode: statusCodes.BAD_REQUEST,
+      message: statusMessages.PROVIDE_REQUIRED_FIELDS,
+    });
+  }
+
+  let user = new userModel({
+    first_name,
+    last_name,
+    email,
+    username,
+    password,
+  });
+
+  user.save((error, success) => {
+    if (error) {
+      return res.status(statusCodes.SERVER_ERROR).json({
+        success: false,
+        statusCodes: statusCodes.SERVER_ERROR,
+        message,
+      });
+    }
+
+    return res.status(statusCodes.CREATED).json({
+      success: true,
+      user: success,
+    });
+  });
 };
