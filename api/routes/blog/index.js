d0e0c55cfdd1283ec184ee4c639f5e0f85e91c35
roqeebYusuff <roqeebyusuff001@gmail.com>
2022-10-31 17:51:52 +0100

    :memo: New author blog route added

--- api/routes/blog/index.js
+++ api/routes/blog/index.js
@@ -5,6 +5,7 @@ const {
   deleteBlog,
   updateBlog,
   getOneBlog,
+  authorBlogs,
 } = require("../../controllers/blog");
 const { authorize } = require("../../middleware/auth");
 
@@ -12,6 +13,7 @@ let router = Router();
 
 router.get("/listOne/:id", getOneBlog); //Get one blog
 router.get("/list", allBlogs); //Get all blog
+router.get("/myBlogs", authorize, authorBlogs); //Author Blogs
 router.post("/create", authorize, createBlog); //Create blog
 router.patch("/update/:id", authorize, updateBlog); //Update blog
 router.delete("/delete/:id", authorize, deleteBlog); //Delete blog
